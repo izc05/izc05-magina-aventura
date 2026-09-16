82261f861245f777f96d0a8a9d1c50a90ea16ef9
IsiVoltPro Mantenimiento <isiciozafracantos@gmail.com>
2026-09-16 06:19:27 +0200

    test(admin): define user moderation and community adapter contract

--- supabase/tests/database/admin_platform_test.sql
+++ supabase/tests/database/admin_platform_test.sql
@@ -1,7 +1,7 @@
 begin;
 
 create extension if not exists pgtap with schema extensions;
-select plan(26);
+select plan(29);
 
 select has_table('public', 'admin_roles', 'admin_roles exists');
 select has_table('public', 'user_admin_roles', 'user_admin_roles exists');
@@ -14,6 +14,7 @@ select has_table('public', 'rewards', 'rewards exists');
 select has_table('public', 'reward_redemptions', 'reward_redemptions exists');
 select has_table('public', 'admin_notifications', 'admin_notifications exists');
 select has_table('public', 'route_safety_incidents', 'route_safety_incidents exists');
+select has_table('public', 'user_moderation_states', 'user_moderation_states exists');
 
 select ok((select relrowsecurity from pg_class where oid = 'public.admin_roles'::regclass), 'RLS admin_roles');
 select ok((select relrowsecurity from pg_class where oid = 'public.user_admin_roles'::regclass), 'RLS user_admin_roles');
@@ -30,6 +31,8 @@ select has_function('public', 'admin_resolve_report', array['uuid','text','text'
 select has_function('public', 'admin_publish_notification', array['uuid'], 'admin_publish_notification exists');
 select has_function('public', 'admin_resolve_safety', array['uuid'], 'admin_resolve_safety exists');
 select has_function('public', 'admin_revoke_role', array['uuid','text'], 'admin_revoke_role exists');
+select has_function('public', 'admin_set_user_state', array['uuid','text','text'], 'admin_set_user_state exists');
+select has_function('public', 'admin_community_overview', array[]::text[], 'admin_community_overview exists');
 
 select * from finish();
 rollback;

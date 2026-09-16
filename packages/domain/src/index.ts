431cf3cab9301c70591844b16287025b72cce524
IsiVoltPro Mantenimiento <isiciozafracantos@gmail.com>
2026-09-16 20:48:04 +0200

    feat: export physical reward reservation API

--- packages/domain/src/index.ts
+++ packages/domain/src/index.ts
@@ -11,6 +11,7 @@ export * from './integration/olive-outbox';
 export * from './olive-tree/olive-tree-progression';
 export * from './rewards/digital-reward-purchase';
 export * from './rewards/olive-grants';
+export * from './rewards/physical-reward-reservation';
 export * from './rewards/reward-catalog';
 export * from './rewards/reward-delivery';
 export * from './rewards/reward-validation';

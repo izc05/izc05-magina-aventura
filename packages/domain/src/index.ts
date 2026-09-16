32e7aef115016bf0c86bc51245ea2c0dd1ccae1f
IsiVoltPro Mantenimiento <isiciozafracantos@gmail.com>
2026-09-16 06:50:03 +0200

    feat(gamification): export badge eligibility API

--- packages/domain/src/index.ts
+++ packages/domain/src/index.ts
@@ -1,5 +1,6 @@
 export * from './collections/collection-progress';
 export * from './gamification/activity-xp';
 export * from './gamification/adventure-stats';
+export * from './gamification/badge-eligibility';
 export * from './gamification/level-progression';
 export * from './routes/route-status';

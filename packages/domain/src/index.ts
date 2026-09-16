5638f2144e0a9cd7dd90550719d8581736075d2a
IsiVoltPro Mantenimiento <isiciozafracantos@gmail.com>
2026-09-16 06:42:54 +0200

    feat(gamification): export adventure stats API

--- packages/domain/src/index.ts
+++ packages/domain/src/index.ts
@@ -1,4 +1,5 @@
 export * from './collections/collection-progress';
 export * from './gamification/activity-xp';
+export * from './gamification/adventure-stats';
 export * from './gamification/level-progression';
 export * from './routes/route-status';

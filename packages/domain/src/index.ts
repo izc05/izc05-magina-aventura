8cb7ede077630c3893037ee38e8f2ff3639b1c62
IsiVoltPro Mantenimiento <isiciozafracantos@gmail.com>
2026-09-16 20:50:44 +0200

    feat: export redemption credential API

--- packages/domain/src/index.ts
+++ packages/domain/src/index.ts
@@ -12,6 +12,7 @@ export * from './olive-tree/olive-tree-progression';
 export * from './rewards/digital-reward-purchase';
 export * from './rewards/olive-grants';
 export * from './rewards/physical-reward-reservation';
+export * from './rewards/redemption-credential';
 export * from './rewards/reward-catalog';
 export * from './rewards/reward-delivery';
 export * from './rewards/reward-validation';

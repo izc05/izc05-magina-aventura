22a2c4068ffac7c72497cccb4bf011533fef230a
IsiVoltPro Mantenimiento <isiciozafracantos@gmail.com>
2026-09-16 20:39:09 +0200

    feat: export Mi Olivo progression API

--- packages/domain/src/index.ts
+++ packages/domain/src/index.ts
@@ -7,6 +7,7 @@ export * from './gamification/level-progression';
 export * from './gamification/progression-cycle';
 export * from './gamification/ranking';
 export * from './integration/olive-outbox';
+export * from './olive-tree/olive-tree-progression';
 export * from './rewards/olive-grants';
 export * from './rewards/reward-delivery';
 export * from './rewards/reward-validation';
